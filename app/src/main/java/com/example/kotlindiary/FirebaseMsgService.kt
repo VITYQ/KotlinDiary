3ca33079452e2093280efa1aec7a7c03951981c2
VITYQ <vitaly.sorokin14@gmail.com>
2020-10-03 20:29:15 +0300

    Добавлен FCM

--- app/src/main/java/com/example/kotlindiary/FirebaseMsgService.kt
+++ app/src/main/java/com/example/kotlindiary/FirebaseMsgService.kt
@@ -5,11 +5,20 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
+import android.util.Log
 import androidx.core.app.NotificationCompat
+import com.google.firebase.iid.internal.FirebaseInstanceIdInternal
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class FirebaseMsgService : FirebaseMessagingService(){
+    override fun onNewToken(p0: String) {
+        var tok = FirebaseMessaging.getInstance()
+        Log.d("ololol", "Togkhjjhken perangkat ini: ${p0}, ${tok}")
+
+        super.onNewToken(p0)
+    }
     override fun onMessageReceived(p0: RemoteMessage) {
         if(p0 != null){
             showNotification(p0.notification?.title, p0.notification?.body)
@@ -24,7 +33,7 @@ class FirebaseMsgService : FirebaseMessagingService(){
 
         val soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this)
-            .setSmallIcon(R.mipmap.ic_launcher)
+            .setSmallIcon(R.drawable.ic_stat_ic_launcher_round_not)
             .setContentTitle(title)
             .setContentText(body)
             .setAutoCancel(true)
